bb59e448562ff30349d7b1b831ee0db1465a9e6b
FumeauxG <54526511+FumeauxG@users.noreply.github.com>
2021-05-16 12:53:40 +0200

    Test automtized meshmixer v4

--- distrib/python/zeTest4.py
+++ distrib/python/zeTest4.py
@@ -1,8 +1,11 @@
 import keyboard
 import time
+import math
 import glob
 import mmapi
 from mmRemote import *;
+
+delay = .200
 
 txtfiles = []
 for file in glob.glob("C:/Gaetan/_Bachelor/mmAPI/mm-api/distrib/python/test/*.stl"):
@@ -118,6 +121,130 @@ if (x<5) or (y<5) or (z<5):
     cmd.AppendCompleteToolCommand("accept")
     remote.runCommand(cmd)
 ###########################################################################
+#angle management
+while True:
+    if keyboard.is_pressed('q'):
+        #rotation in degrees
+        rX = math.radians(-90)
+        rY = 0
+        rZ = 0
+
+        #convert degrees to the transformation vector stuff
+        r1 = math.cos(rY ) * math.cos(rZ)
+        r2 = math.sin(rZ)
+        r3 = -math.sin(rY) * math.cos(rZ)
+        r4 = -math.cos(rY) * math.sin(rZ) * math.cos(rX) + math.sin(rY) * math.sin(rX)
+        r5 = math.cos(rZ) * math.cos(rX)
+        r6 = math.sin(rY) * math.sin(rZ) * math.cos(rX) + math.cos(rY) * math.sin(rX)
+        r7 = math.cos(rY) * math.sin(rZ) * math.sin(rX) + math.sin(rY) * math.cos(rX)
+        r8 = -math.cos(rZ) * math.sin(rX)
+        r9 = -math.sin(rY) * math.sin(rZ) * math.sin(rX) + math.cos(rY) * math.cos(rX)
+
+        cmd.AppendBeginToolCommand("transform")
+        cmd.AppendToolParameterCommand("rotation",r1,r2,r3,r4,r5,r6,r7,r8,r9)
+        cmd.AppendCompleteToolCommand("accept")
+        remote.runCommand(cmd)
+        time.sleep(delay)
+    if keyboard.is_pressed('a'):
+        #rotation in degrees
+        rX = 0
+        rY = math.radians(-90)
+        rZ = 0
+
+        #convert degrees to the transformation vector stuff
+        r1 = math.cos(rY ) * math.cos(rZ)
+        r2 = math.sin(rZ)
+        r3 = -math.sin(rY) * math.cos(rZ)
+        r4 = -math.cos(rY) * math.sin(rZ) * math.cos(rX) + math.sin(rY) * math.sin(rX)
+        r5 = math.cos(rZ) * math.cos(rX)
+        r6 = math.sin(rY) * math.sin(rZ) * math.cos(rX) + math.cos(rY) * math.sin(rX)
+        r7 = math.cos(rY) * math.sin(rZ) * math.sin(rX) + math.sin(rY) * math.cos(rX)
+        r8 = -math.cos(rZ) * math.sin(rX)
+        r9 = -math.sin(rY) * math.sin(rZ) * math.sin(rX) + math.cos(rY) * math.cos(rX)
+
+        cmd.AppendBeginToolCommand("transform")
+        cmd.AppendToolParameterCommand("rotation",r1,r2,r3,r4,r5,r6,r7,r8,r9)
+        cmd.AppendCompleteToolCommand("accept")
+        remote.runCommand(cmd)
+        time.sleep(delay)       
+    if keyboard.is_pressed('y'):
+        #rotation in degrees
+        rX = 0
+        rY = 0
+        rZ = math.radians(-90)
+
+        #convert degrees to the transformation vector stuff
+        r1 = math.cos(rY ) * math.cos(rZ)
+        r2 = math.sin(rZ)
+        r3 = -math.sin(rY) * math.cos(rZ)
+        r4 = -math.cos(rY) * math.sin(rZ) * math.cos(rX) + math.sin(rY) * math.sin(rX)
+        r5 = math.cos(rZ) * math.cos(rX)
+        r6 = math.sin(rY) * math.sin(rZ) * math.cos(rX) + math.cos(rY) * math.sin(rX)
+        r7 = math.cos(rY) * math.sin(rZ) * math.sin(rX) + math.sin(rY) * math.cos(rX)
+        r8 = -math.cos(rZ) * math.sin(rX)
+        r9 = -math.sin(rY) * math.sin(rZ) * math.sin(rX) + math.cos(rY) * math.cos(rX)
+
+        cmd.AppendBeginToolCommand("transform")
+        cmd.AppendToolParameterCommand("rotation",r1,r2,r3,r4,r5,r6,r7,r8,r9)
+        cmd.AppendCompleteToolCommand("accept")
+        remote.runCommand(cmd)
+        time.sleep(delay)
+        
+  
+    if keyboard.is_pressed('1'):
+        cmd = mmapi.StoredCommands()
+        cmd.AppendBeginToolCommand("units")
+        key = cmd.AppendGetToolParameterCommand("worldX")
+        remote.runCommand(cmd)
+        result_val = mmapi.any_result()
+        bFound = cmd.GetToolParameterCommandResult(key, result_val)
+
+        if bFound:
+            if result_val.type == 0:
+                x = result_val.f
+            elif result_val.type == 1:
+                x = result_val.i
+            elif result_val.type == 2:
+                x = result_val.b
+            elif result_val.type == 3:
+                x = (result_val.x, result_val.y, result_val.z)
+            elif result_val.type == 4:
+                x = result_val.m
+        key = cmd.AppendGetToolParameterCommand("worldY")
+        remote.runCommand(cmd)
+        result_val = mmapi.any_result()
+        bFound = cmd.GetToolParameterCommandResult(key, result_val)
+
+        if bFound:
+            if result_val.type == 0:
+                y = result_val.f
+            elif result_val.type == 1:
+                y = result_val.i
+            elif result_val.type == 2:
+                y = result_val.b
+            elif result_val.type == 3:
+                y = (result_val.x, result_val.y, result_val.z)
+            elif result_val.type == 4:
+                y = result_val.m
+        key = cmd.AppendGetToolParameterCommand("worldZ")
+        remote.runCommand(cmd)
+        result_val = mmapi.any_result()
+        bFound = cmd.GetToolParameterCommandResult(key, result_val)
+
+        if bFound:
+            if result_val.type == 0:
+                z = result_val.f
+            elif result_val.type == 1:
+                z = result_val.i
+            elif result_val.type == 2:
+                z = result_val.b
+            elif result_val.type == 3:
+                z = (result_val.x, result_val.y, result_val.z)
+            elif result_val.type == 4:
+                z = result_val.m        
+        cmd.AppendCompleteToolCommand("accept")
+        remote.runCommand(cmd)
+        break
 
 # align the shape in the center
 # construct commands to run
@@ -139,24 +266,48 @@ bFound = cmd.GetSceneCommandResult_FindObjectByName(cmd_key, result_val)
 print(result_val.i)
 mainId = result_val.i;
 
-# read the open file
-pathObject = "C:/Gaetan/_Bachelor/mmAPI/mm-api/distrib/python/objet/cube.stl"
-pathTemp = pathObject.split("\\")
-print(pathTemp)
+#Cube or cylender
+while True:
+    if keyboard.is_pressed('1'):
+        # read the open file
+        pathObject = "C:/Gaetan/_Bachelor/mmAPI/mm-api/distrib/python/objet/cube.stl"
+        pathTemp = pathObject.split("\\")
+        print(pathTemp)
 
-#import cube
-cmd = mmapi.StoredCommands()
-key = cmd.AppendSceneCommand_AppendMeshFile(pathObject);
-remote.runCommand(cmd)
+        #import cube
+        cmd = mmapi.StoredCommands()
+        key = cmd.AppendSceneCommand_AppendMeshFile(pathObject);
+        remote.runCommand(cmd)
 
-#id cube
-cmd = mmapi.StoredCommands()
-cmd_key = cmd.AppendSceneCommand_FindObjectByName("cube.stl")
-remote.runCommand(cmd)
-result_val = mmapi.any_result()
-bFound = cmd.GetSceneCommandResult_FindObjectByName(cmd_key, result_val)
-print(result_val.i)
-supportId = result_val.i;
+        #id cube
+        cmd = mmapi.StoredCommands()
+        cmd_key = cmd.AppendSceneCommand_FindObjectByName("cube.stl")
+        remote.runCommand(cmd)
+        result_val = mmapi.any_result()
+        bFound = cmd.GetSceneCommandResult_FindObjectByName(cmd_key, result_val)
+        print(result_val.i)
+        supportId = result_val.i;
+        break;
+    if keyboard.is_pressed('2'):
+        # read the open file
+        pathObject = "C:/Gaetan/_Bachelor/mmAPI/mm-api/distrib/python/objet/cylender.stl"
+        pathTemp = pathObject.split("\\")
+        print(pathTemp)
+
+        #import cylender
+        cmd = mmapi.StoredCommands()
+        key = cmd.AppendSceneCommand_AppendMeshFile(pathObject);
+        remote.runCommand(cmd)
+
+        #id cylender
+        cmd = mmapi.StoredCommands()
+        cmd_key = cmd.AppendSceneCommand_FindObjectByName("cylender.stl")
+        remote.runCommand(cmd)
+        result_val = mmapi.any_result()
+        bFound = cmd.GetSceneCommandResult_FindObjectByName(cmd_key, result_val)
+        print(result_val.i)
+        supportId = result_val.i;
+        break;
 
 v = mmapi.vec3f()
 v.x = x
@@ -164,7 +315,7 @@ v.y = y
 v.z = z
 
 cmd.AppendBeginToolCommand("transform")
-cmd.AppendToolParameterCommand("scale",v)
+cmd.AppendToolParameterCommand("dimensions",v)
 cmd.AppendCompleteToolCommand("accept")
 remote.runCommand(cmd)
 
@@ -179,65 +330,148 @@ remote.runCommand(cmd);
 
 cmd.AppendBeginToolCommand("transform")
 remote.runCommand(cmd)
-scaleX = 1
-scaleY = 1
-scaleZ = 1
-v.x = 1
-v.y = 1
-v.z = 1
+
+
+translate = mmapi.vec3f()
+
+translate.x = 0
+translate.y = 0
+translate.z = 0
+
+xTemp = x
+x = z 
+z = y 
+y = xTemp 
 
 while True:
     if keyboard.is_pressed('q'):
-        v.x = 1 + 1/x
-        v.y = 1
-        v.z = 1
-        cmd.AppendToolParameterCommand("scale",v)
+        x = x + 1
+        v.x = x
+        v.y = y
+        v.z = z
+        cmd.AppendToolParameterCommand("dimensions",v)
         remote.runCommand(cmd)
-        x = x + 1;
-        time.sleep(.300)
+        time.sleep(delay)
     if keyboard.is_pressed('w'):
-        v.x = 1 - 1/x
-        v.y = 1
-        v.z = 1
-        cmd.AppendToolParameterCommand("scale",v)
+        x = x - 1
+        v.x = x
+        v.y = y
+        v.z = z
+        cmd.AppendToolParameterCommand("dimensions",v)
         remote.runCommand(cmd)
-        x = x - 1;
-        time.sleep(.300)
+        time.sleep(delay)
         
     if keyboard.is_pressed('a'):
-        v.y = 1 + 1/y
-        v.x = 1
-        v.z = 1
-        cmd.AppendToolParameterCommand("scale",v)
+        y = y + 1
+        v.x = x
+        v.y = y
+        v.z = z
+        cmd.AppendToolParameterCommand("dimensions",v)
         remote.runCommand(cmd)
-        y = y + 1;
-        time.sleep(.300)
+        time.sleep(delay)
     if keyboard.is_pressed('s'):
-        v.y = 1 - 1/y
-        v.x = 1
-        v.z = 1
-        cmd.AppendToolParameterCommand("scale",v)
+        y = y - 1
+        v.x = x
+        v.y = y
+        v.z = z
+        cmd.AppendToolParameterCommand("dimensions",v)
         remote.runCommand(cmd)
-        y = y - 1;
-        time.sleep(.300)
+        time.sleep(delay)
         
     if keyboard.is_pressed('y'):
-        v.z = 1 + 1/z
-        v.x = 1
-        v.y = 1
-        cmd.AppendToolParameterCommand("scale",v)
+        z = z + 1
+        v.x = x
+        v.y = y
+        v.z = z
+        cmd.AppendToolParameterCommand("dimensions",v)
         remote.runCommand(cmd)
-        z = z + 1;
-        time.sleep(.300)
+        time.sleep(delay)
     if keyboard.is_pressed('x'):
-        v.z = 1 - 1/z
-        v.x = 1
-        v.y = 1
-        cmd.AppendToolParameterCommand("scale",v)
+        z = z - 1
+        v.x = x
+        v.y = y
+        v.z = z
+        cmd.AppendToolParameterCommand("dimensions",v)
         remote.runCommand(cmd)
-        z = z - 1;
-        time.sleep(.300)
+        time.sleep(delay)
+
+
+    if keyboard.is_pressed('e'):
+        translate.x = 1
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        translate.x = 0
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        time.sleep(delay)
+    if keyboard.is_pressed('r'):
+        translate.x = -1
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        translate.x = 0
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        time.sleep(delay)
         
+    if keyboard.is_pressed('d'):
+        translate.x = 0
+        translate.y = 1
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        translate.x = 0
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        time.sleep(delay)
+    if keyboard.is_pressed('f'):
+        translate.x = 0
+        translate.y = -1
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        translate.x = 0
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        time.sleep(delay)
+        
+    if keyboard.is_pressed('c'):
+        translate.x = 0
+        translate.y = 0
+        translate.z = 1
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        translate.x = 0
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        time.sleep(delay)
+    if keyboard.is_pressed('v'):
+        translate.x = 0
+        translate.y = 0
+        translate.z = -1
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        translate.x = 0
+        translate.y = 0
+        translate.z = 0
+        cmd.AppendToolParameterCommand("translationWorld",translate)
+        remote.runCommand(cmd)
+        time.sleep(delay)
+        
+  
     if keyboard.is_pressed('1'):
         cmd.AppendCompleteToolCommand("accept")
         remote.runCommand(cmd)
